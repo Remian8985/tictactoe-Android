644974e4745b38f3631a4128390a64c7563c6ed8
Tasdiq Ameem <ameem.is.online@gmail.com>
2015-07-26 20:55:01 -0400

    button layout is done

--- tictactoe_game_ui_tester/app/src/main/java/com/tasdeeq/tictactoe_game_ui_tester/GameWindow.java
+++ tictactoe_game_ui_tester/app/src/main/java/com/tasdeeq/tictactoe_game_ui_tester/GameWindow.java
@@ -1,38 +1,321 @@
 package com.tasdeeq.tictactoe_game_ui_tester;
 
+import android.annotation.TargetApi;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.view.ViewTreeObserver;
 
 
 public class GameWindow extends ActionBarActivity {
+
+	final String TAG = "com.tasdeeq.tictactoe_game_ui_tester";
+	// CREATE GAME MODE OBJECTS HERE
+	// ^classes aren't added to project yet
+
+
+	// TO DO:
+	// on touch and on press for each of 9 buttons		**done**
+	// on touch check if the place is empty (if yes return false)
+	// ^if false, initiate background processing and place piece
+	// Visually place piece
+
+	// Finally:
+	// Make buttons invisible with no text
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game_window);
+
+		final ImageView gridImage = (ImageView) findViewById(R.id.gridImage);
+		ViewTreeObserver vto = gridImage.getViewTreeObserver();
+
+		// Get critical length of grid image and calculate button lengths from there:
+		vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+			public boolean onPreDraw() {
+				gridImage.getViewTreeObserver().removeOnPreDrawListener(this);
+				int imageWidth = gridImage.getMeasuredWidth();
+				Log.d(TAG, "imageView width : " + imageWidth);		// debug code
+
+				int buttonLength = getButtonLength(imageWidth) ;
+				resizeButtons(buttonLength);
+
+				return true;
+			}
+		});
+
+		// Set on touch listeners for the buttons:
+		// touch listeners are  for debugging purpose only.
+		// Everything can be handled by the easier click listener
+		Button button00 = (Button) findViewById(R.id.button00);
+		Button button01 = (Button) findViewById(R.id.button01);
+		Button button02 = (Button) findViewById(R.id.button02);
+		Button button10 = (Button) findViewById(R.id.button10);
+		Button button11 = (Button) findViewById(R.id.button11);
+		Button button12 = (Button) findViewById(R.id.button12);
+		Button button20 = (Button) findViewById(R.id.button20);
+		Button button21 = (Button) findViewById(R.id.button21);
+		Button button22 = (Button) findViewById(R.id.button22);
+
+		button00.setOnTouchListener(onTouchListener);
+		button01.setOnTouchListener(onTouchListener);
+		button02.setOnTouchListener(onTouchListener);
+		button10.setOnTouchListener(onTouchListener);
+		button11.setOnTouchListener(onTouchListener);
+		button12.setOnTouchListener(onTouchListener);
+		button20.setOnTouchListener(onTouchListener);
+		button21.setOnTouchListener(onTouchListener);
+		button22.setOnTouchListener(onTouchListener);
+
+
+	//	button00.setEnabled(false);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_game_window, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
+		private void buttonOnClick(View v) {
+			int id = v.getId();
+			Drawable imageDrawable = getDrawableRes(true);
+			Log.d(TAG, "Button clicked : " + id);		// debug code
+			switch(v.getId()){
+				case R.id.button00:
+					//DO something
+					v.setEnabled(false);
+					break;
+				case R.id.button01:
+					Button temp = (Button) findViewById(R.id.button00);
+					temp.performClick();
+					break;
+				case R.id.button02:
+					//DO something
+					break;
+				case R.id.button10:
+					//DO something
+					break;
+				case R.id.button11:
+					//DO something
+					break;
+				case R.id.button12:
+					//DO something
+					break;
+				case R.id.button20:
+					//DO something
+					break;
+				case R.id.button21:
+					//DO something
+					break;
+				case R.id.button22:
+					//DO something
+					break;
+				default:
+					//DO something
+					break;
+			}
+
 		}
 
-		return super.onOptionsItemSelected(item);
+
+
+
+
+ 	// This touch listener is for debugging purpose only.
+	// Everything can be handled by the easier click listener
+	private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			// Must return false
+
+			int id = v.getId();
+			if (event.getAction() == MotionEvent.ACTION_DOWN ) { // button is pressed "down"
+				boolean isInvalid = true;
+
+				switch (v.getId()) {
+					case R.id.button00:
+						isInvalid =  isOccupied(0, 0);
+						break;
+					case R.id.button01:
+						isInvalid =  isOccupied(0, 1);
+						break;
+					case R.id.button02:
+						isInvalid =  isOccupied(0, 2);
+						break;
+					case R.id.button10:
+						isInvalid =  isOccupied(1, 0);
+						break;
+					case R.id.button11:
+						isInvalid =  isOccupied(1, 1);
+						break;
+					case R.id.button12:
+						isInvalid =  isOccupied(1, 2);
+						break;
+					case R.id.button20:
+						isInvalid =  isOccupied(2, 0);
+						break;
+					case R.id.button21:
+						isInvalid =  isOccupied(2, 1);
+						break;
+					case R.id.button22:
+						isInvalid =  isOccupied(2, 2);
+						break;
+					default:
+						isInvalid =  true;
+				}
+				Log.d(TAG, "Button touched : " + id);		// debug code
+				return false;
+			}
+			else {
+				Log.d(TAG, "Button released : " + id);		// debug code
+				return false;
+			}
+		}
+	};
+
+
+	private Drawable getDrawableRes(boolean player1Move){
+		final String noughtStr = "@drawable/nought";
+		final String crossStr = "@drawable/cross";
+		String uri = noughtStr; // default , player1 move
+		if (!player1Move)
+			uri = crossStr;
+
+		int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+		Drawable res;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+			res = getDrawable(imageResource);
+		else	// because the following is deprecated apparently
+			res = getResources().getDrawable(imageResource);
+
+		return res;
 	}
+
+
+
+
+
+
+
+
+	// Changes all the input buttons to be the right size
+	private void resizeButtons(int length) {
+		// Yes, I know things are repeated here, I suck.
+
+		Button button00 = (Button) findViewById(R.id.button00);
+		Button button01 = (Button) findViewById(R.id.button01);
+		Button button02 = (Button) findViewById(R.id.button02);
+		Button button10 = (Button) findViewById(R.id.button10);
+		Button button11 = (Button) findViewById(R.id.button11);
+		Button button12 = (Button) findViewById(R.id.button12);
+		Button button20 = (Button) findViewById(R.id.button20);
+		Button button21 = (Button) findViewById(R.id.button21);
+		Button button22 = (Button) findViewById(R.id.button22);
+
+		adjustButtonSize(button00, length, length);
+		adjustButtonSize(button01, length, length);
+		adjustButtonSize(button02, length, length);
+		adjustButtonSize(button10, length, length);
+		adjustButtonSize(button11, length, length);
+		adjustButtonSize(button12, length, length);
+		adjustButtonSize(button20, length, length);
+		adjustButtonSize(button21, length, length);
+		adjustButtonSize(button22, length, length);
+	}
+
+
+	// ONLY FOR RELATIVE LAYOUT!
+	private void adjustButtonSize(Button button, int height, int width) {
+		RelativeLayout.LayoutParams buttonParams = (RelativeLayout.LayoutParams) button.getLayoutParams();
+		buttonParams.height = height;
+		buttonParams.width = width;
+		button.setLayoutParams(buttonParams);
+	}
+
+	private int getButtonLength(int args){
+		return args/3;
+	}
+
+	private boolean isOccupied(int i, int j){
+		// TO DO
+		return false; // PLACEHOLDER!
+	}
+
+	private void aiPlacePiece(int i, int j){
+		Button targetSlotButton = getButton(i, j);
+		if (targetSlotButton == null)
+			return;
+
+		// Visual updates need to be done onClick
+		// Slot information updates need to be done onClick
+		targetSlotButton.performClick();
+	}
+
+
+	private Button	getButton (int i, int j){
+		int viewID = -1;			 // unassigned
+		int	errorVerifier = viewID;
+		if (i == 0){
+			switch (j){
+				case 0:
+					viewID = R.id.button00;
+					break;
+				case 1:
+					viewID = R.id.button02;
+					break;
+				case 2:
+					viewID = R.id.button02;
+					break;
+				default:
+					break;
+			}
+		}
+		else if (i == 1){
+			switch (j){
+				case 0:
+					viewID = R.id.button10;
+					break;
+				case 1:
+					viewID = R.id.button12;
+					break;
+				case 2:
+					viewID = R.id.button12;
+					break;
+				default:
+					break;
+			}
+		}
+		else if (i == 2){
+			switch (j){
+				case 0:
+					viewID = R.id.button20;
+					break;
+				case 1:
+					viewID = R.id.button22;
+					break;
+				case 2:
+					viewID = R.id.button22;
+					break;
+				default:
+					break;
+			}
+		}
+		Button button;
+		if (viewID == errorVerifier)
+			button = null;
+		else
+			button = (Button) findViewById(viewID);
+
+		return button;
+	}
+
+
 }
